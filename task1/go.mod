570497d1cc3a3e83c14d73c9fe9e556c0f3e0b2a
Your Name <you@example.com>
2024-09-11 15:33:35 +0300

    add post handler

---
+++ task1/go.mod
@@ -0,0 +1,5 @@
+module github.com/Resolution-hash/course-pantela/task1
+
+go 1.22.3
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- task1/main.go
+++ task1/main.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+
+	"encoding/json"
+
+	"github.com/gorilla/mux"
+)
+
+/*TODO:
+1. Если еще нет, создать аккаунт на [github](https://github.com/). Создать репозиторий и загрузить туда проект
+2. Добавить глобальную переменную `message`
+3. Добавить `POST` handler, который будет принимать json с полем `message` и записывать его содержимое в нашу переменную.
+4. Обновить `GET` handler, чтобы он возвращал “hello, `message` ”
+5. Сделать коммит и загрузить изменения в созданный заранее репозиторий
+6. Прислать мне ссылку на ваш репозиторий
+7. Также прислать мне скриншоты кода и постмана :)
+*/
+
+type Message struct {
+	Text string `json:"text"`
+}
+
+var message Message
+
+// type Message struct{
+// 	Text string `json:"text"`
+// }
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "hello, ", message.Text)
+
+}
+
+func MessageHandler(w http.ResponseWriter, r *http.Request) {
+	data, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintln(w, err)
+		return
+	}
+
+	err = json.Unmarshal(data, &message)
+	if err != nil {
+		fmt.Fprintln(w, err)
+		return
+	}
+	fmt.Println("message:", message.Text)
+}
 
 func main() {
-	fmt.Println("Hello")
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
+	router.HandleFunc("/api/message", MessageHandler).Methods("POST")
+
+	fmt.Println("Server is starting on port:8080")
+	http.ListenAndServe(":8080", router)
 }
